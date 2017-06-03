ac36dda0073e737b6594f50fdeaa753f036d4729
chinotyanmofumofu <chinotyanmofumofu@gmail.com>
2017-06-04 02:58:26 +0900

    0258

---
+++ android/study/src/study/Sutudy2.java
@@ -0,0 +1,23 @@
+package study;
+
+public class Sutudy2 {
+	public static void main(String[] arg){
+		Study s = new Study();
+		s.a = 200;
+		s.b = 810;
+		s.x = "world";
+		int chino = s.sss(12, 5);
+		String rize = s.eee("Hello");
+		System.out.println(chino);
+		System.out.println(rize);
+		s.shownum(114514);
+	}
+	
+		
+		
+		
+		
+	
+	}
+
+
