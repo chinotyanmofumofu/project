50e5a0c8babf19aff1e4adaa666255d6032ff562
chinotyanmofumofu <chinotyanmofumofu@gmail.com>
2017-06-04 03:13:53 +0900

    313

--- java/study/src/study/Study.java
+++ java/study/src/study/Study.java
@@ -1,23 +1,5 @@
 package study;
 
-public class Study {
-	int a = 100;
-	int b ;
-	String x = "Hello";
-	int sss(int a , int b){
-		return a*b;
-		
-	}
-	String eee(String g){
-		return g+this.x;
-	}
-	void shownum(int E ){
-		System.out.println("number="+E);
-		System.out.println("number2="+b);
-		System.out.println("chinotyanmofumofu");
-		
-		
-		
-	}
+public class study {
 
 }
