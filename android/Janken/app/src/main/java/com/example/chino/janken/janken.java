570a33d78e7aaf6a9c4bbc6dec21f8d42d7e18fe
chinotyanmofumofu <chinotyanmofumofu@gmail.com>
2017-06-05 17:22:45 +0900

    no message

--- android/Janken/app/src/main/java/com/example/chino/janken/janken.java
+++ android/Janken/app/src/main/java/com/example/chino/janken/janken.java
@@ -1,7 +1,9 @@
 package com.example.chino.janken;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -11,6 +13,11 @@ public class janken extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janken);
+        SharedPreferences resultdata = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = resultdata.edit();
+        editor.clear();
+        editor.commit();
+
     }
     public void jankenpad (View view){
         Intent A = new Intent (this, result.class);
